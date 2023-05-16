cee5c88c3a7bc608e7ad1be809e3a41552ae63b7
LETTNER <benipapapapa123@gmail.com>
2023-05-16 13:50:00 +0200

    retries eingefügt

--- client.py
+++ client.py
@@ -2,10 +2,17 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("81.169.141.81", 8888))
+#client.connect(("localhost", 8888))
 
 
-message = client.recv(1024).decode()
-client.send(input(message).encode())
-message = client.recv(1024).decode()
-client.send(input(message).encode())
-print(client.recv(1024).decode())
+while True:
+
+    message = client.recv(1024).decode()
+    client.send(input(message).encode())
+    message = client.recv(1024).decode()
+    client.send(input(message).encode())
+    response = client.recv(1024).decode()
+    print(response)
+
+    if response == "Login successful!":
+        break
--- server.py
+++ server.py
@@ -5,28 +5,33 @@ import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("81.169.141.81", 8888))
+#server.bind(("localhost", 8888))
 
 server.listen()
 
 
 def handle_connection(c):
-    c.send("Username: ".encode())
-    username = c.recv(1024).decode()
-    c.send("Password: ".encode())
-    password = c.recv(1024)
-    password = hashlib.sha256(password).hexdigest()
+    for i in range(3):  # Versuche insgesamt 3 Anmeldeversuche
+        c.send("Username: ".encode())
+        username = c.recv(1024).decode()
+        c.send("Password: ".encode())
+        password = c.recv(1024).decode()
+        password = hashlib.sha256(password.encode()).hexdigest()
 
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
+        conn = sqlite3.connect("database.db")
+        cur = conn.cursor()
 
-    cur.execute("SELECT * FROM userdata WHERE username = ? AND password = ?", (username, password))
+        cur.execute("SELECT * FROM userdata WHERE username = ? AND password = ?", (username, password))
 
-    if cur.fetchall():
-        c.send("Login succesful!".encode())
-        print("Login succesful")
-    else:
-        c.send("Login failed!".encode())
-        print("Login failed")
+        if cur.fetchall():
+            c.send("Login successful!".encode())
+            break  # Beenden Sie die Schleife, wenn die Anmeldung erfolgreich war
+        else:
+            c.send("Login failed! Please try again.".encode())
+
+    c.send("Too many tries please retry later".encode())
+    c.close()
+
 
 while True:
     client, addr = server.accept()
