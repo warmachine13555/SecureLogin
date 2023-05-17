93b478af5247f3df7290a98654a19adbbea3bbfb
LETTNER <benipapapapa123@gmail.com>
2023-05-17 09:41:12 +0200

    anpassung

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#client.connect(("81.169.141.81", 8888))
-client.connect(("localhost", 7777))
+client.connect(("81.169.141.81", 8888))
+#client.connect(("localhost", 7777))
 
 
 while True:
