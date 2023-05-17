b9020a52f4669b5957dceb70dfc66728a33ccd22
LETTNER <benipapapapa123@gmail.com>
2023-05-17 09:48:03 +0200

    anpassung

--- authentication.py
+++ authentication.py
@@ -3,6 +3,7 @@ import hashlib
 import socket
 import threading
 import pyotp
+import qrcode
 from totpgen import totp_register
 
 
@@ -23,7 +24,27 @@ def auth(c):
             c.send("Login successful!".encode())
             if cur.execute("SELECT * FROM userdata WHERE username = ? AND password = ? AND totp = 0", (username,
                                                                                                      password)):
-                totp_register(c)
+                def totp_register(c):
+                    key = pyotp.random_base32()  # Generate a random base32 key
+
+                    print(key)
+
+                    totp = pyotp.TOTP(key)
+
+                    uri = totp.provisioning_uri(name="Benjamin", issuer_name="Online Banking")
+
+                    # Manually append the TOTP period to the URI
+                    uri += "&period=30"
+
+                    qrcode.make(uri).save("totp.png")
+
+                    totp = pyotp.TOTP(key)
+
+                    while True:
+                        c.send("Enter MFA Code: ".encode())
+                        mfa_code = c.recv(1024).decode()
+                        verify = totp.verify(mfa_code)
+                        print(verify)
 
         else:
             c.send("Login failed! Please try again.".encode())
