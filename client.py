bc7c43a4327e7bc7a91a2080d69eb35e340d8f86
LETTNER <benipapapapa123@gmail.com>
2023-05-25 12:04:22 +0200

    Anpassung client.py

--- client.py
+++ client.py
@@ -4,19 +4,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #client.connect(("81.169.141.81", 8888))
 client.connect(("localhost", 7777))
 
+message = client.recv(1024).decode()
+client.send(input(message).encode())
 
 while True:
-
     message = client.recv(1024).decode()
     client.send(input(message).encode())
-    message = client.recv(1024).decode()
-    client.send(input(message).encode())
-    response = client.recv(1024).decode()
-    print(response)
-
-    if response == "Login successful!":
-        message = client.recv(1024).decode()
-        client.send(input(message).encode())
-        message = client.recv(1024).decode()
-        client.send(input(message).encode())
-        break
