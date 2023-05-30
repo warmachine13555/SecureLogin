908ddfd3024ce6b4b3dbf3c4f692f0cc851165ac
LETTNER <benipapapapa123@gmail.com>
2023-05-30 10:04:53 +0200

    anpassung

--- totpgen.py
+++ totpgen.py
@@ -1,12 +1,10 @@
-import time
 import pyotp
 import qrcode
-import socket
-import threading
 
 
 
-key = pyotp.random_base32()  # Generate a random base32 key
+
+key = pyotp.random_base32()
 
 print(key)
 
@@ -14,7 +12,7 @@ totp = pyotp.TOTP(key)
 
 uri = totp.provisioning_uri(name="Benjamin", issuer_name="Online Banking")
 
-# Manually append the TOTP period to the URI
+
 uri += "&period=30"
 
 qrcode.make(uri).save("totp.png")
